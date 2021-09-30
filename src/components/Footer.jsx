5fc56417edac9414c9b0231bb8d38ad8cc0d17c5
evan-kapantais <evankapantais@gmail.com>
2021-09-30 14:14:43 +0300

    style the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,75 @@
 import React from "react";
+import { Link } from "gatsby";
+import Social from "./Social";
+import { StaticQuery, graphql } from "gatsby";
 
 const Footer = () => {
+  const getLink = edge =>
+    `/#${edge.node.title.toLowerCase().replace(/\s/g, "-")}`;
+
   return (
-    <footer id="layout-footer">
-      <p>© Wolt, {new Date().getFullYear()}</p>
-      <p>
-        Build by{" "}
-        <a href="https://www.instagram.com/evans.webworks">Evan Kapantais</a>
-      </p>
-    </footer>
+    <StaticQuery
+      query={graphql`
+        query Footer {
+          allStrapiSection {
+            edges {
+              node {
+                title
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <footer id="layout-footer">
+          <div className="footer-top">
+            <div className="footer-container">
+              <h2>Πριν φύγεις</h2>
+              <div className="footer-top__content">
+                <section>
+                  <h3>Ενότητες</h3>
+                  <ul>
+                    {data.allStrapiSection.edges.map((edge, i) => (
+                      <li key={i}>
+                        <Link className="footer-link" to={getLink(edge)}>
+                          {edge.node.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+                <div>
+                  <section>
+                    <h3>Η Wolt</h3>
+                    <p>
+                      H Wolt είναι η κορυφαία πλατφόρμα online delivery σε
+                      πολλές χώρες της Ευρώπη και Ασίας. Iδρύθηκε στη Φινλανδία
+                      το 2014 και ήδη λειτουργεί σε 23 χώρες και πάνω από 150
+                      πόλεις.
+                    </p>
+                  </section>
+                  <section>
+                    <h3>Ακολούθησέ μας</h3>
+                    <Social />
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <div className="footer-container">
+              <p>© Wolt, {new Date().getFullYear()}</p>
+              <p>
+                Built with <span className="heart">❤️</span> by{" "}
+                <Link to="https://instagram.com/evans.webworks">
+                  Evan Kapantais
+                </Link>
+              </p>
+            </div>
+          </div>
+        </footer>
+      )}
+    />
   );
 };
 
