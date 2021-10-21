7d9912f9e22aa33db8e274c0ae1b8811dd86fb64
evan-kapantais <evankapantais@gmail.com>
2021-10-21 17:54:36 +0300

    omit news section if there are no news

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -7,39 +7,43 @@ const NewsSection = ({ newsItems }) => {
   const isGreekPage =
     typeof window !== "undefined" && window.location.pathname === "/";
 
-  const areThereNews =
-    (newsItems.length === 1 && newsItems[0].node.title !== "empty") ||
-    newsItems[0].node.title === " ";
+  const areThereNews = newsItems.length > 1 && newsItems[0].node.title !== "";
 
   return (
-    <section id="news">
-      <StaticImage
-        src="../images/phone.png"
-        alt="phone"
-        className="phone-deco"
-        data-nofocus
-      />
-      <div className="news-container">
-        <h1 className="news-title">{isGreekPage ? "Τα Νέα Μας" : "News"}</h1>
-        {areThereNews ? (
-          newsItems.map((newsItem, i) => (
-            <React.Fragment key={i}>
-              <NewsItem newsItem={newsItem} />
-              <hr />
-            </React.Fragment>
-          ))
-        ) : isGreekPage ? (
-          <p>
-            Δεν υπάρχουν νέα αυτή τη στιγμή. Μπορείς να συνεχίσεις{" "}
-            <Link to="#select-topic">παρακάτω</Link>.
-          </p>
-        ) : (
-          <p>
-            There's no news at the moment. You can{" "}
-            <Link to="#select-topic">move on</Link>.
-          </p>
-        )}
-      </div>
+    <section id="news" data-news={areThereNews ? true : false}>
+      {areThereNews && (
+        <>
+          <StaticImage
+            src="../images/phone.png"
+            alt="phone"
+            className="phone-deco"
+            data-nofocus
+          />
+          <div className="news-container">
+            <h1 className="news-title">
+              {isGreekPage ? "Τα Νέα Μας" : "News"}
+            </h1>
+            {areThereNews ? (
+              newsItems.map((newsItem, i) => (
+                <React.Fragment key={i}>
+                  <NewsItem newsItem={newsItem} />
+                  <hr />
+                </React.Fragment>
+              ))
+            ) : isGreekPage ? (
+              <p>
+                Δεν υπάρχουν νέα αυτή τη στιγμή. Μπορείς να συνεχίσεις{" "}
+                <Link to="#select-topic">παρακάτω</Link>.
+              </p>
+            ) : (
+              <p>
+                There's no news at the moment. You can{" "}
+                <Link to="#select-topic">move on</Link>.
+              </p>
+            )}
+          </div>
+        </>
+      )}
     </section>
   );
 };
