13fc2f28a0cecaabd79c08ad24bc8e16a10f3bcf
evan-kapantais <evankapantais@gmail.com>
2021-10-28 16:02:28 +0300

    add 404 pages for malta and cyprus

---
+++ src/pages/malta/404.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "gatsby";
+import Seo from "../../components/seo";
+
+const NotFoundPage = () => {
+  return (
+    <section className="fof-page">
+      <Seo title="404" />
+      <div className="fof-container">
+        <h1 className="fof-title">
+          <span className="fof-emoji">😕</span>
+          <br />
+          Oops!
+        </h1>
+        <p className="fof-subtitle">
+          Looks like the page you requested does not exist. <br />
+          <code>404: Not Found</code>
+        </p>
+        <Link to="/malta">We think you should go back home now.</Link>
+      </div>
+    </section>
+  );
+};
+
+export default NotFoundPage;
